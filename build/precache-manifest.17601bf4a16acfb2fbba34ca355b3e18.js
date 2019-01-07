ca897b83dad5847c86cb5659e532982432d64d41
gto2020 <george_to@me.com>
2019-01-07 00:01:55 -0800

    initial commit

---
+++ build/precache-manifest.17601bf4a16acfb2fbba34ca355b3e18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6b9ba1b77f2062cbba5a",
+    "url": "/static/js/main.6b9ba1b7.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "6b9ba1b77f2062cbba5a",
+    "url": "/static/css/main.f84b49f9.chunk.css"
+  },
+  {
+    "revision": "8f7c13354327f978df13f7f382af4497",
+    "url": "/index.html"
+  }
+];
